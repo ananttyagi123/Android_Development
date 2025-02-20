ea33c40a6597d36859de39f8b5c715fef4135cbe
Anant Tyagi <ananttyagi359@gmail.com>
2025-02-20 13:55:58 +0530

    Toolbar Custom menu 2.0

--- app/src/main/java/com/example/android20/MainActivity.kt
+++ app/src/main/java/com/example/android20/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.android20
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +12,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+            return super.onCreateOptionsMenu(menu)
+            menuInflater.inflate(R.menu.custom_menu,menu)
+            return true
+        }
         }
     }
 }
