eb42d3ba8daa04d865726c8f094ddc7671927a4b
Anant Tyagi <ananttyagi359@gmail.com>
2025-02-25 13:50:25 +0530

    Toolbar Custom menu 2.0

--- app/src/main/java/com/example/android20/MainActivity.kt
+++ app/src/main/java/com/example/android20/MainActivity.kt
@@ -1,22 +1,55 @@
 package com.example.android20
 
 import android.os.Bundle
+
 import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-            return super.onCreateOptionsMenu(menu)
-            menuInflater.inflate(R.menu.custom_menu,menu)
-            return true
-        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.custom_menu, menu)
+        return true
+
+        // when you don;t write the code in kt file then while writing the kt file for custom menu for target action using menu
+        //  menuInflater.inflate()
+        // it allows you to create menu or list of items
+        // R.custom_menu.menu is the custom_menu.xml file  then it connects sto the kt file
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.Search -> {
+                Toast.makeText(this, "Search is Selected", Toast.LENGTH_SHORT).show()
+                return true
+            }
+
+            R.id.Whatsapp -> {
+                Toast.makeText(this, "WhatsApp is Selected", Toast.LENGTH_SHORT).show()
+                return true
+            }
+
+            R.id.Facebook -> {
+                Toast.makeText(this, "Facebook is Selected", Toast.LENGTH_SHORT).show()
+                return true
+            }
+
+            R.id.Instagram -> {
+                Toast.makeText(this, "Instagram is Selected", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            // default option
+            else->super.onOptionsItemSelected(item)
         }
     }
 }
+
